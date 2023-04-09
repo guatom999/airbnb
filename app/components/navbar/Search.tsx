551c9645c88daec76f0651c141162d39b40ca782
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-04-10 03:02:49 +0700

    add Search Bar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,7 +1,51 @@
+'use client'
+
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    return (
+        <div
+            className="border-[1px]
+        w-full
+        md:w-auto
+        py-2
+        rounded-full
+        shadow-sm
+        hover:shadow-md
+        transition
+        cursor-pointer"
+        >
+            <div
+                className="
+                flex
+                flex-row
+                items-center
+                justify-between
+                "
+            >
+                <div
+                    className="
+                    text-sm
+                    font-semibold
+                    px-6"
+                >
+                    Search
+                </div>
+                <div
+                    className="
+                        hidden
+                        sm:block
+                        text-sm
+                        font-semibold
+                        px-6
+                        border-x-[1px]
+                        flex-1
+                        text-center
+                    "
+                >
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Search

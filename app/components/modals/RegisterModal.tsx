385f192210701d4ede8829a41c8a148ecd65fb5b
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-04-21 17:43:58 +0700

    Update

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,14 +4,16 @@ import axios from 'axios'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react'
-import {
-    FieldValues,
+import { 
+    FieldValues, 
     SubmitHandler,
     useForm
-} from 'react-hook-form'
+  } from "react-hook-form";
 
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -27,6 +29,7 @@ const RegisterModal = () => {
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
+        console.log("Coming to Here")
 
         axios.post('/api/register', data).then(() => {
             registerModal.onClose();
@@ -36,6 +39,43 @@ const RegisterModal = () => {
             setIsLoading(false)
         })
     }
+
+    const bodyContent = (
+        <div
+            className="flex flex-col gap-4"
+        >
+            <Heading
+                title="Bossza"
+                subtitle='create account'
+            />
+            <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            /> 
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="password"
+                type="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            {/* Hello Modal Body! */}
+        </div>
+    )
     return (
         <Modal
             disabled={isLoading}
@@ -44,6 +84,7 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
         />
     )
 }
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import Avartar from '../Avartar';
 import { useState, useCallback } from 'react'
 import MenuItem from './MenuItems';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+    const registerModal =  useRegisterModal()
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -77,7 +79,7 @@ const UserMenu = () => {
                                 label="Login"
                             />
                             <MenuItem
-                                onClick={() => { }}
+                                onClick={registerModal.onOpen}
                                 label="Sign up"
                             />
                         </>
